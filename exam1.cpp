077eae02073771652a25cb2c0a4232a0760ef783
Unknown <tongnarumon1998@gmail.com>
2018-02-22 12:34:01 +0700

    1

--- exam1.cpp
+++ exam1.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <stdio.h>
+#include <conio.h>
+using namespace std;
+int main(void)
+{
+int ();
+    printf("Wellcome to CLI Calculator V1.00/n");
 
-int main() {
+    int a,b;
+    cout << "menu" <<endl;
+    cout << " 1.+ " <<endl;
+    cout << " 2.- " <<endl;
+    cout << " 3.* " <<endl;
+    cout << " 4./ " <<endl;
+    cout << " quit " <<endl;
 
         return 0;
 }
